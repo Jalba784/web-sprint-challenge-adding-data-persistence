85228c71d1dd32813d22437ad05d33ab946cf84a
josepaniagua <jpanjr@gmail.com>
2020-07-07 17:31:07 -0700

    added cleanup file

--- migrations/20200707165901_projects.js
+++ migrations/20200707165901_projects.js
@@ -1,7 +1,27 @@
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", tbl => {
+    tbl.increments("id");
+    tbl.string("project_name");
+    tbl.text("description");
+    tbl.boolean("completed").defaultTo(false);
+  });
+
+  await knex.schema.createTable("tasks", tbl => {
+    tbl.increments("id");
+    tbl.text("description").notNullable();
+    tbl.text("notes");
+    tbl.boolean("completed").defaultTo(false);
+  });
+
+  await knex.schema.createTable("resources", tbl => {
+    tbl.increments("id");
+    tbl.string("resource_name").notNullable();
+    tbl.text("description");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("projects");
 };
---
+++ seeds/001-cleanup.js
@@ -0,0 +1,5 @@
+exports.seed = async function(knex) {
+  await knex("resources").truncate();
+  await knex("tasks").truncate();
+  await knex("projects").truncate();
+};
