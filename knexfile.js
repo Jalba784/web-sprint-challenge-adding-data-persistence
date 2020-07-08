42f6feaa3771d3a6ab2d3d436cdc3b277e9366b9
josepaniagua <jpanjr@gmail.com>
2020-07-07 18:06:24 -0700

    had to rollback missing some code

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,20 @@
 module.exports = {
   development: {
     client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./data/projects.db3"
     },
-    useNullAsDefault: true
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   }
 };
